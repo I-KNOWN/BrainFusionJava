270b3cf19439d70db9ff1ca210cc8a30f32f2c27
John Pandian <ben10friend14@gmail.com>
2023-08-28 12:28:24 +0530

    Added image get call in interface

--- app/src/main/java/com/example/brainfusion/apiInterface/ApiService.java
+++ app/src/main/java/com/example/brainfusion/apiInterface/ApiService.java
@@ -1,9 +1,13 @@
 package com.example.brainfusion.apiInterface;
 
+import static com.example.brainfusion.utils.Constants.BASEAPIIMAGE;
 import static com.example.brainfusion.utils.Constants.ORIGIN;
 import static com.example.brainfusion.utils.Constants.REFERER;
+import static com.example.brainfusion.utils.Constants.SEC_CH_UA;
+import static com.example.brainfusion.utils.Constants.USERAGENT;
 import static com.example.brainfusion.utils.Constants.UUIDENDPOINT;
 
+import com.example.brainfusion.model.ImageResponseModel;
 import com.example.brainfusion.model.UidModel;
 
 import okhttp3.RequestBody;
@@ -12,8 +16,11 @@ import retrofit2.http.Body;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface ApiService {
+
+
     @Headers({
             "Accept: */*",
             "Accept-Language: en-US,en;q=0.9,en-GB;q=0.8",
@@ -22,11 +29,26 @@ public interface ApiService {
             "Origin: "+ORIGIN,
             "Pragma: no-cache",
             "Referer: "+REFERER,
-            "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-            "sec-ch-ua: \"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Google Chrome\";v=\"114\""
+            "User-Agent: "+USERAGENT,
+            "sec-ch-ua: "+SEC_CH_UA
     })
     @POST(UUIDENDPOINT)
-    Call<UidModel> runQuery(@Header("Content-Type") String data, @Body RequestBody requestBody);
+    Call<UidModel> runQuery(@Header("Content-Type") String data, @Body RequestBody requestBody); // Returns UUID
+
+
+    @Headers({
+            "Accept: */*",
+            "Accept-Language: en-US,en;q=0.9,en-GB;q=0.8",
+            "Cache-Control: no-cache",
+            "Connection: keep-alive",
+            "Pragma: no-cache",
+            "Referer: "+REFERER,
+            "Sec-Fetch-Site: same-origin",
+            "User-Agent: "+USERAGENT,
+            "sec-ch-ua: "+SEC_CH_UA
+    })
+    @POST(BASEAPIIMAGE+"/{uuid}/")
+    Call<ImageResponseModel> getImageBase(@Path("uuid") String uuid); // Returns base64(Image)
 
 
 
--- app/src/main/java/com/example/brainfusion/utils/Constants.java
+++ app/src/main/java/com/example/brainfusion/utils/Constants.java
@@ -13,4 +13,8 @@ public class Constants {
     //IMAGE
     public static final String BASEAPIIMAGE = BASEAPI+"/status/";
 
+
+    public static final String USERAGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36";
+    public static final String SEC_CH_UA = "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Google Chrome\";v=\"114\"";
+
 }
